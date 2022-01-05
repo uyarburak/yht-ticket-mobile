a63c02f9b3ae0b36c1e851f0af543e96ab3ae7c0
uyarburak <burakuyar25@gmail.com>
2022-01-05 20:40:48 +0300

    Initiliaze onesignal

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:yht_ticket/app_binding.dart';
 import 'package:yht_ticket/di.dart';
 import 'package:yht_ticket/modules/not_found/not_found_screen.dart';
@@ -14,6 +15,29 @@ void main() async {
   await DenpendencyInjection.init();
 
   await initializeDateFormatting('tr_TR', null);
+
+  //Remove this method to stop OneSignal Debugging
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+  OneSignal.shared.setAppId("4042c904-a960-4692-b889-511bfcdee887");
+
+  // Setting External User Id with Callback Available in SDK Version 3.9.3+
+  OneSignal.shared.setExternalUserId("burak").then((results) {
+    print(results.toString());
+  }).catchError((error) {
+    print(error.toString());
+  });
+
+  // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+  OneSignal.shared.promptUserForPushNotificationPermission().then((accepted) {
+    print("Accepted permission: $accepted");
+  });
+
+  OneSignal.shared
+      .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+    // Will be called whenever a notification is opened/button pressed.
+    print(result.notification.rawPayload);
+  });
 
   runApp(App());
   configLoading();
